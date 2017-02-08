e96611eb6c2532bfff1425365f5ddb3a5fa35fb2
3010a14 <3010a14@guanabana.ece.uprm.edu>
2017-02-08 17:44:41 -0400

    Lo que se hizo en el lab 3. so far lo que haz hecho de la parte 6 do it jijijijiijijijijijijjiji <3

---
+++ MyFirstProject/src/Lab3Exercises.java
@@ -0,0 +1,16 @@
+package src;
+import javax.swing.JFrame;
+
+public class Lab3Exercises {
+	public static void main(String[] args) {
+		JFrame myFrame = new JFrame(" Kristalys Ruiz ");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(850, 400);
+		myFrame.setSize(200, 200);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
+		myFrame.setVisible(true);
+		
+		
+	}
+}
